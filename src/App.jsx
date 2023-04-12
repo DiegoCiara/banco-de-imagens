06fcc2e20d50f35ce82049d01767c48fb86723b0
DiegoCiara <suporte.diegociara@gmail.com>
2023-04-12 14:59:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Image1 from '/src/assets/1.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+        <img src={Image1}/>
+    </div>
+  )
+}
+
+export default App

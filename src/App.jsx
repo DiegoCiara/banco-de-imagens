9a5b202969982a365af3d014206be4ae4a438131
Diego Antonio <suporte.diegociara@gmail.com>
2023-05-10 19:58:32 -0300

    Base Wave Slides

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,17 @@ import Image10 from '/src/assets/10.png'
 import Image11 from '/src/assets/11.png'
 import Image12 from '/src/assets/12.png'
 import Image13 from '/src/assets/13.png'
+
+import Wave1 from'/src/assets/SiteWave/1.png'
+import Wave2 from'/src/assets/SiteWave/2.png'
+import Wave3 from'/src/assets/SiteWave/3.png'
+import Wave4 from'/src/assets/SiteWave/4.png'
+import Wave5 from'/src/assets/SiteWave/5.png'
+import Wave6 from'/src/assets/SiteWave/6.png'
+import Wave7 from'/src/assets/SiteWave/7.png'
+import Wave8 from'/src/assets/SiteWave/8.png'
+
+
 import './App.css'
 
 function App() {
@@ -35,6 +46,16 @@ function App() {
         <img src={Image11}/>
         <img src={Image12}/>
         <img src={Image13}/>
+        <img src={Wave1}/>
+        <img src={Wave2}/>
+        <img src={Wave3}/>
+        <img src={Wave4}/>
+        <img src={Wave5}/>
+        <img src={Wave6}/>
+        <img src={Wave7}/>
+        <img src={Wave8}/>
+
+
     </div>
   )
 }
